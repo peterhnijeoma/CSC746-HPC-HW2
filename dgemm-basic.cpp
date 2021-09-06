1b5f2dfdc3fc1f1f7298c32163fa301c17abe2ef
peterhnijeoma <ijeomapeter55@gmail.com>
2021-09-06 12:13:49 -0700

    fix view

--- dgemm-basic.cpp
+++ dgemm-basic.cpp
@@ -12,7 +12,7 @@ void square_dgemm(int n, double* A, double* B, double* C)
 {
    // display matrices
    std::cout << "matrix A elements - row major" << '\n';
-   for (int i = 0; i < n; i++)
+   for (int i = 0; i < n; i+=2)
    {
       for (int j = 0; j < n; j++)
       {
@@ -58,7 +58,7 @@ void square_dgemm(int n, double* A, double* B, double* C)
    std::cout << "matrix B elements - col major" << '\n';
    for (int i = 0; i < n; i++)
    {
-      for (int j = 0; j < n; j++)
+      for (int j = 0; j < n; j+=2)
       {
          std::cout << B[j] << " ";
       }
