21d108abb6c3a5b6409df845b5a2474075852ea7
peterhnijeoma <ijeomapeter55@gmail.com>
2021-09-06 14:12:20 -0700

    test mm

--- dgemm-basic.cpp
+++ dgemm-basic.cpp
@@ -64,17 +64,19 @@ void square_dgemm(int n, double* A, double* B, double* C)
       }
       std::cout << '\n';
    }
-   
+
    
    for (int arow = 0; arow < n; arow++)
    {
       for (int bcol = 0; bcol < n; bcol++)
       {
-         C[arow] = 0.0;
+         //C[arow] = 0.0;
          for (int i = 0; i < n; i++)
          {
-            C[arow] += A[arow] * B[bcol];
+            double cvalue += A[i*n+bcol] * B[bcol+i];
+            std::cout << "A value = " << A[i*n+bcol] << " B value = " << B[bcol+i] << '\n';
          }
+         std::cout << "cvalue = " << cvalue << '\n';
       }      
    }
    
