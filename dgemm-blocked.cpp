2fe14fe8bf44b68b96d08eb9a29caf00eafc62a0
peterhnijeoma <ijeomapeter55@gmail.com>
2021-09-09 10:58:43 -0700

    block code 1a

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -10,8 +10,8 @@ const char* dgemm_desc = "Blocked dgemm.";
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
 {
   int ii, kk, jj;     // block row and col indexes
-  double cvalue;
-  int en = (n/block_size); /* number of blocks */
+  double cvalue;      // accumulator
+  int num_blocks = (n/block_size); /* number of blocks; n is matrix size nXn*/
 
   // display matrices
   std::cout << "A matrix - linear" << '\n';
@@ -21,35 +21,81 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
   }
   std::cout << "\n\n";
 
+  // copy column major vector A into 2D array
+  double AA[n][n];
+  for (int i = 0; i < n*n; i+=n)
+  {
+     for (int j = 0; i < n; i++)
+     {
+        AA[j][i] = A[i+j];
+     }
+     
+  }
+  
+
   std::cout << "B matrix - linear" << '\n';
   for (int i = 0; i < n*n; i++)
   {
     std::cout << B[i] << " ";
   }
   std::cout << "\n\n";
-   
-  for (ii = 0; ii < en; kk++)
+
+  // copy column major vector B into 2D array
+  double BB[n][n];
+  for (int i = 0; i < n*n; i+=n)
   {
-    for (jj = 0; jj < en; jj++)
+     for (int j = 0; i < n; i++)
+     {
+        BB[j][i] = B[i+j];
+     }
+     
+  }
+  
+  // copy column major vector C into 2D array
+  double CC[n][n];
+  for (int i = 0; i < n*n; i+=n)
+  {
+     for (int j = 0; i < n; i++)
+     {
+        CC[j][i] = C[i+j];
+     }
+     
+  }
+  
+  // block matrix multiplication logic
+  for (ii = 0; ii < n; ii += block_size)  // partition rows by block size; iterate for n/block_size blocks
+  {
+    for (jj = 0; jj < n; jj += block_size) // partition columns by block size; iterate for n/block_size blocks
     {
-      for (kk = 0; kk < en; kk++)
+      for (kk = 0; kk < n; kk += block_size)  // for each row and column of blocks
       {
-        for (int arow = ii; arow < ii + block_size; arow += block_size)
+        // basic matrix multiple applied to multiple blocks
+        for (int arow = ii; arow < ii + block_size; arow++)
         {
           for (int bcol = jj; bcol < jj + block_size; bcol++)
           {
-             cvalue = 0.0; //C[i][j];
+             //cvalue = 0.0; //C[i][j];
              for (int k = kk; k < kk + block_size; k++)
              {
-                cvalue += A[arow+k] * B[bcol+k]; // A[arow+k*n] * B[bcol*n+k]; A[i][k] *B [k][j];
+                CC[arow][bcol] = CC[arow][k] += A[arow][k] * B[k][bcol];
              }
              //C[bcol+arow] C[bcol*n+arow]
-             std::cout << "C[arow+bcol]- C[" << arow << "][" << bcol << "] = " << cvalue << '\n';
-             C[arow+bcol*n] = cvalue;
+             //std::cout << "C[arow+bcol]- C[" << arow << "][" << bcol << "] = " << cvalue << '\n';
+             //C[arow+bcol*n] = cvalue;
           }
         }
       }
     }
+  }
+  
+  // copy 2d array CC to column major vector C
+  for (int i = 0; i < n; i++)
+  {
+     for (int j = 0; j < n; j++)
+     {
+        C[i*n+j] = CC[j][i];
+     }
+     
   }
   
   std::cout << "\nC linear \n";
